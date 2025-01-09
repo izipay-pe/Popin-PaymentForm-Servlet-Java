687af7bda3fc86d8aedbf44be650f351a2dba28b
izipay-pe <103148193+izipay-pe@users.noreply.github.com>
2025-01-09 15:41:46 -0500

    Update McwServlet.java

--- src/main/java/com/example/McwServlet.java
+++ src/main/java/com/example/McwServlet.java
@@ -27,19 +27,20 @@ public class McwServlet extends HttpServlet {
     public void init() throws ServletException {
 	// Configuración para las plantillas Thymeleaf
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(getServletContext());
-        templateResolver.setPrefix("/WEB-INF/templates/");
-        templateResolver.setSuffix(".html");
-	templateResolver.setCharacterEncoding("UTF-8");
-        templateResolver.setTemplateMode("HTML");
-
+        templateResolver.setPrefix("/WEB-INF/templates/"); // Ubicación de las pantillas
+        templateResolver.setSuffix(".html"); // Extensión que tienen los archivos para la plantilla
+	templateResolver.setCharacterEncoding("UTF-8"); // Codificación de los caracteres
+        templateResolver.setTemplateMode("HTML"); // Modo de procesamiento de plantillas
+	
 	// Iniciando Thymeleaf
         this.templateEngine = new TemplateEngine();
         this.templateEngine.setTemplateResolver(templateResolver);
-
+	
 	// Iniciando las propiedades y el controlador
 	properties = new McwProperties();
 	mcwController = new McwController();
     }
+    
 
     /**
      * @@ Manejo de rutas GET @@
@@ -50,11 +51,11 @@ public class McwServlet extends HttpServlet {
         
 	// Obtiene la ruta solicitada
         String path = request.getServletPath();
-	// Creación del contexto para pasar los valores para la plantilla
-        WebContext context = new WebContext(request, response, getServletContext());
+        // Creación del contexto para pasar los valores para la plantilla
+	WebContext context = new WebContext(request, response, getServletContext());
 	
 	// Generar orderId
-	String orderId = mcwController.generarOrderId();
+	String orderId = mcwController.generateOrderId();
         
 	// Procesamiento según la ruta solicitada
 	switch (path) {
@@ -76,6 +77,7 @@ public class McwServlet extends HttpServlet {
                 break;
         }
     }
+
 
     /**
      * @@ Manejo de rutas POST @@
@@ -111,21 +113,24 @@ public class McwServlet extends HttpServlet {
             	}
 		
 		//Obtener PublicKey
-		String publicKey = properties.getProperty("publicKey");
+		String publicKey = properties.getProperty("PUBLIC_KEY");
 
 		//Obtenemos el FormToken generado
-		String formToken = mcwController.generarToken(parameters);
+		String formToken = mcwController.generateFormToken(parameters);
 		
 		//Agrerar el token en el contexto para ser usado en el template
 		context.setVariable("formToken", formToken);
 		context.setVariable("publicKey", publicKey);
-
+		
 		// Renderizando el template y enviando los datos agregados al contexto
 		templateEngine.process("checkout", context, response.getWriter());	
 
 		break;		
 
             case "/result":
+
+		String HMAC_SHA256 = properties.getProperty("HMAC_SHA256");
+		
 		// Asignando los valores de la respuesta de Izipay en las variables
 		krHash = request.getParameter("kr-hash");
         	krHashAlgorithm = request.getParameter("kr-hash-algorithm");
@@ -133,65 +138,58 @@ public class McwServlet extends HttpServlet {
         	krAnswer = request.getParameter("kr-answer");
         	krHashKey = request.getParameter("kr-hash-key");
 		
+		// Válida que la respuesta sea íntegra comprando el hash recibido en el 'kr-hash' con el generado con el 'kr-answer'
+		if (!mcwController.checkHash(krHash, HMAC_SHA256, krAnswer)){
+			break;
+		}
+
 		// Almacenamos los datos del kr-answer
 		jsonResponse = new JSONObject(krAnswer);
-		// Convertimos el valor del JSON obtenido a 'pretty print' para su visualización en el template
 		String pJson = jsonResponse.toString(4);
-
+		
 		// Almacenamos los datos del pago en las variables
 		orderStatus = jsonResponse.getString("orderStatus");
         	int orderTotalAmount = jsonResponse.getJSONObject("orderDetails").getInt("orderTotalAmount");
         	orderId = jsonResponse.getJSONObject("orderDetails").getString("orderId");
 		String currency = jsonResponse.getJSONObject("orderDetails").getString("orderCurrency");
 
-		// Formatear el valor de 'amount' de centimos a decimales
     		double orderAmountdouble = (double) orderTotalAmount / 100;
     		String orderAmount = String.format("%.02f", orderAmountdouble);
-    
 
-		// Válida que la data POST que obtiene /result tenga los valores esperados
-		if (krHash != null && krHashAlgorithm != null && krAnswerType != null && krAnswer != null && krHashKey != null) {
-			// Válida que la respuesta sea íntegra comprando el hash recibido en el 'kr-hash' con el generado con el 'kr-answer'
-			boolean isValidKey = mcwController.checkHash(krHash, krHashKey, krAnswer);
+								
+		// Establece variables en el contexto para la plantilla	
+		context.setVariable("krHash", krHash);
+                context.setVariable("krHashAlgorithm", krHashAlgorithm);
+                context.setVariable("krAnswerType", krAnswerType);
+                context.setVariable("krAnswer", krAnswer);
+                context.setVariable("krHashKey", krHashKey);
+		context.setVariable("pJson", pJson);
+		context.setVariable("orderStatus", orderStatus);
+		context.setVariable("orderTotalAmount", orderAmount);
+		context.setVariable("orderId", orderId);
+		context.setVariable("currency", currency);
 			
-			// Procesa la condicional si la firma es correcta
-			if (isValidKey) {
-			// Establece variables en el contexto para la plantilla			
-			context.setVariable("krHash", krHash);
-                	context.setVariable("krHashAlgorithm", krHashAlgorithm);
-                	context.setVariable("krAnswerType", krAnswerType);
-                 	context.setVariable("krAnswer", krAnswer);
-                	context.setVariable("krHashKey", krHashKey);
-			context.setVariable("pJson", pJson);
-			context.setVariable("orderStatus", orderStatus);
-			context.setVariable("orderTotalAmount", orderAmount);
-			context.setVariable("orderId", orderId);
-			context.setVariable("currency", currency);
+		// Renderizando el template y enviando los datos agregados al contexto
+		templateEngine.process("result", context, response.getWriter());
 			
-			// Renderizando el template y enviando los datos agregados al contexto
-			templateEngine.process("result", context, response.getWriter());
-			
-			break;
-
-			} else {
-				break;
-			}
-
-		} else {
-        		break;
-		}
+		break;
 
 	    
 	    case "/ipn":
+
+		String PASSWORD = properties.getProperty("PASSWORD");
+
 		// Asignando los valores de la respuesta IPN en las variables
 		krHash = request.getParameter("kr-hash");
         	krAnswer = request.getParameter("kr-answer");
-        	krHashKey = request.getParameter("kr-hash-key");
 		
+		// Válida que la respuesta sea íntegra comprando el hash recibido en el 'kr-hash' con el generado con el 'kr-answer'
+		if (!mcwController.checkHash(krHash, PASSWORD, krAnswer)){
+			System.out.println("Notification Error");
+		}
+
 		// Procesa la respuesta del pago
 		jsonResponse = new JSONObject(krAnswer);
-		
-		// Ejemplo de extracción de datos
 		JSONArray transactionsArray = jsonResponse.getJSONArray("transactions");
 		JSONObject transactions = transactionsArray.getJSONObject(0);
 		
@@ -199,17 +197,9 @@ public class McwServlet extends HttpServlet {
 		orderStatus = jsonResponse.getString("orderStatus");
 		orderId = jsonResponse.getJSONObject("orderDetails").getString("orderId");
 		String uuid = transactions.getString("uuid");
-
-		// Válida que la respuesta sea íntegra comprando el hash recibido en el 'kr-hash' con el generado con el 'kr-answer'
-		boolean isValidKey = mcwController.checkHash(krHash, krHashKey, krAnswer);
 		
-		// Procesa la condicional si la firma es correcta
-		if (isValidKey) {
-			// Imprimiendo en el log el Order Status
-			System.out.println("OK! Order Status is " + orderStatus);
-		} else {
-			System.out.println("Notification Error");
-		}
+		// Retornando el OrderStatus
+		response.getWriter().write("OK! Order Status is " + orderStatus);
 
 		break;
 	    
@@ -219,4 +209,3 @@ public class McwServlet extends HttpServlet {
         }
     }
 }
-
